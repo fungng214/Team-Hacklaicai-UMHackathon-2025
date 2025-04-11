dc54106b220b1aa3c370d45054cd76e91a647053
fungng214 <cfng.my@gmail.com>
2025-04-12 00:19:05 +0800

    Add files via upload

---
+++ transaction_data.py
@@ -0,0 +1,43 @@
+from data import Data
+
+class Transaction_Data(Data): 
+    def __init__(self):
+        self.data = {}
+        self.read_file()
+   
+    def add(self, key, value):
+        if key in self.data:
+            raise ValueError("Item already exists in data")
+        self.data[key] = value 
+        
+    
+    def get(self, key):
+        return key + key.values()
+        
+    def remove(self, item):
+        if item in self.data:
+            del self.data[item]
+        else:
+            raise ValueError("Item not found in data")  
+    
+    def duplicate(self, item):
+        if item in self.data:
+            self.data.append(item)
+        else:
+            raise ValueError("Item not found in data")
+    
+    def read_file(self):
+        with open("keywords.csv", "r") as file:
+            readlines = file.readlines()
+            for x in readlines:
+                index,order_id,order_time,driver_arrival_time,driver_pickup_time,delivery_time,order_value,eater_id,merchant_id = x.split(",")
+                self.data[order_id] = {
+                    "index": index,
+                    "order_time": order_time,
+                    "driver_arrival_time": driver_arrival_time,
+                    "driver_pickup_time": driver_pickup_time,
+                    "delivery_time": delivery_time,
+                    "order_value": order_value,
+                    "eater_id": eater_id,
+                    "merchant_id": merchant_id
+                }
